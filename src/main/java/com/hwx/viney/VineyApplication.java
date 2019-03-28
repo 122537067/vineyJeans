9869b575cec4236e44eb7e6e6ad55bb583efa452
122537067 <122537067@qq.com>
2019-03-28 23:48:38 +0800

    testVersion

---
+++ src/main/java/com/hwx/viney/VineyApplication.java
@@ -0,0 +1,13 @@
+package com.hwx.viney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VineyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VineyApplication.class, args);
+    }
+
+}
